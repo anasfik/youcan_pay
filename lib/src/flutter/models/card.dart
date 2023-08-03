0061e7dfde84921a7b6af21f601b0501228d9727
Mohammed Anas FIKHI <work@gwhyyy.com>
2023-08-03 16:11:19 +0100

    [ Add ] added cards factories for testing payments

--- lib/src/flutter/models/card.dart
+++ lib/src/flutter/models/card.dart
@@ -28,6 +28,46 @@ class YouCanPayCard extends Equatable {
     required this.expireDate,
   });
 
+  factory YouCanPayCard.testingNo3dsSuccessCard([String cardHolderName = '']) {
+    return _baseTestCard(
+      creditCard: 4242424242424242,
+      cardHolderName: cardHolderName,
+    );
+  }
+
+  factory YouCanPayCard.testing3dsSuccessCard([String cardHolderName = '']) {
+    return _baseTestCard(
+      creditCard: 4000000000003220,
+      cardHolderName: cardHolderName,
+    );
+  }
+
+  factory YouCanPayCard.testing3dsRejectedCard([String cardHolderName = '']) {
+    return _baseTestCard(
+      creditCard: 4000008400001629,
+      cardHolderName: cardHolderName,
+    );
+  }
+
+  factory YouCanPayCard.testingNo3dsNoFundsCard([String cardHolderName = '']) {
+    return _baseTestCard(
+      creditCard: 4000000000000077,
+      cardHolderName: cardHolderName,
+    );
+  }
+
+  static YouCanPayCard _baseTestCard({
+    required int creditCard,
+    required String cardHolderName,
+  }) {
+    return YouCanPayCard(
+      creditCard: creditCard,
+      cardHolderName: cardHolderName,
+      cvv: 112,
+      expireDate: YouCanPayExpireDate(month: 12, year: 2024),
+    );
+  }
+
   @override
   List<Object?> get props => [
         creditCard,
